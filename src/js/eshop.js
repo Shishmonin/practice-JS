d461902294142ee75179b76d44dcb1ed5d513741
Sergey Shishmonin <shishmonin-s@mail.ru>
2019-08-27 10:36:02 +0300

    Create data json
    change style  coffee card

--- src/js/eshop.js
+++ src/js/eshop.js
@@ -1,8 +1,17 @@
+let check = {};
+
+
 $('document').ready(function(){
-    loadGoods();
+    loadClassic();
+    loadCoffeeDrink();
+    loadIceCoffee();
+    loadCocoa();
+
+    testCheck();
+
 });
 
-function loadGoods() {
+function loadClassic() {
     //загружаю товары на страницу
     $.getJSON('classicCoffee.json', function (data) {
         //console.log(data);
@@ -18,10 +27,136 @@ function loadGoods() {
             out+='<p>Цена: '+data[key]['cost'] + ' руб' + '</p>' + '<p>Объем: '+data[key]['ml']+ ' ml' + '</p>';
             out+='</div>';
 
-            out+='<button><p>Добавить</p></button>';
+            out+='<button class="add-to-check" data-art="'+key+'"><p>Добавить</p></button>';
 
             out+='</div>';
         }
-        $('#goods').html(out);
+        $('#classic-content').html(out);
+        // Новые строчки
+        $('button.add-to-check').on('click', addToCheck);
+        $('button.add-to-check').on('click', showCheck);
+
     })
 }
+
+// Новая функция добавления в счет за кофе
+function addToCheck() {
+    // добавление позиции в счет
+    let articul = $(this).attr('data-art');
+    // Проверка на количество товара при нажатии на кнопку
+    if (check[articul] != undefined) {
+        check[articul]++;
+    }else {
+        check[articul] = 1;
+    }
+    // сохранение в localStorage
+    localStorage.setItem('check', JSON.stringify(check) );
+    console.log(check);
+    showCheck();
+}
+function testCheck() {
+    // Проверка на существующие позиции счета в localStorage
+    if (localStorage.getItem('check') != null) {
+        check = JSON.parse (localStorage.getItem('check'));
+    }
+}
+
+function showCheck() {
+    let out = '';
+    for (let key in check){
+        out+='<img src="'+classicCoffee[key].image+'">';
+    }
+    $('.calc-check___list-add-drinks').html(out);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function loadCoffeeDrink() {
+    //загружаю товары на страницу
+    $.getJSON('coffeeDrink.json', function (data) {
+        //console.log(data);
+        let out = '';
+        for (let key in data){
+            out+='<div class="card-drink d-flex" >';
+            out+='<img src="'+data[key].image+'">';
+
+            out+='<h2>'+data[key]['name']+'</h2>';
+            out+='<p>'+ data[key]['compose']+ '</p>';
+
+            out+='<div>';
+            out+='<p>Цена: '+data[key]['cost'] + ' руб' + '</p>' + '<p>Объем: '+data[key]['ml']+ ' ml' + '</p>';
+            out+='</div>';
+
+            out+='<button class="add-to-check" data-art="'+key+'"><p>Добавить</p></button>';
+
+            out+='</div>';
+        }
+        $('#coffeedrink-content').html(out);
+    })
+}
+
+function loadIceCoffee() {
+    //загружаю товары на страницу
+    $.getJSON('iceCoffee.json', function (data) {
+        //console.log(data);
+        let out = '';
+        for (let key in data){
+            out+='<div class="card-drink d-flex" >';
+            out+='<img src="'+data[key].image+'">';
+
+            out+='<h2>'+data[key]['name']+'</h2>';
+            out+='<p>'+ data[key]['compose']+ '</p>';
+
+            out+='<div>';
+            out+='<p>Цена: '+data[key]['cost'] + ' руб' + '</p>' + '<p>Объем: '+data[key]['ml']+ ' ml' + '</p>';
+            out+='</div>';
+
+            out+='<button class="add-to-check" data-art="'+key+'"><p>Добавить</p></button>';
+
+            out+='</div>';
+        }
+        $('#icecoffee-content').html(out);
+    })
+}
+
+function loadCocoa() {
+    //загружаю товары на страницу
+    $.getJSON('cocoa.json', function (data) {
+        //console.log(data);
+        let out = '';
+        for (let key in data){
+            out+='<div class="card-drink d-flex" >';
+            out+='<img src="'+data[key].image+'">';
+
+            out+='<h2>'+data[key]['name']+'</h2>';
+            out+='<p>'+ data[key]['compose']+ '</p>';
+
+            out+='<div>';
+            out+='<p>Цена: '+data[key]['cost'] + ' руб' + '</p>' + '<p>Объем: '+data[key]['ml']+ ' ml' + '</p>';
+            out+='</div>';
+
+            out+='<button class="add-to-check" data-art="'+key+'"><p>Добавить</p></button>';
+
+            out+='</div>';
+        }
+        $('#cocoa-content').html(out);
+    })
+}
+
