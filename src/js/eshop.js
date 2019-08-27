5aa6fa5d56424b2e70404b7c03a930d546a77b0a
Sergey Shishmonin <shishmonin-s@mail.ru>
2019-08-27 16:17:27 +0300

    Show list in check

--- src/js/eshop.js
+++ src/js/eshop.js
@@ -8,6 +8,7 @@ $('document').ready(function(){
     loadCocoa();
 
     testCheck();
+    showCheck();
 
 });
 
@@ -27,22 +28,24 @@ function loadClassic() {
             out+='<p>Цена: '+data[key]['cost'] + ' руб' + '</p>' + '<p>Объем: '+data[key]['ml']+ ' ml' + '</p>';
             out+='</div>';
 
-            out+='<button class="add-to-check" data-art="'+key+'"><p>Добавить</p></button>';
+            out+='<button class="add-to-check" data-atr="'+key+'" data-cost="'+data[key]['cost']+'" data-name="'+data[key]['name']+'"><p>Добавить</p></button>';
 
             out+='</div>';
         }
         $('#classic-content').html(out);
         // Новые строчки
         $('button.add-to-check').on('click', addToCheck);
-        $('button.add-to-check').on('click', showCheck);
 
     })
 }
 
-// Новая функция добавления в счет за кофе
+// Сохранение в локалстор
 function addToCheck() {
     // добавление позиции в счет
-    let articul = $(this).attr('data-art');
+    // let articul = $(this).attr('data-atr');
+    let articul = $(this).attr('data-name');
+
+    // check[ $(this).attr('data-cost'), $(this).attr('data-name')];
     // Проверка на количество товара при нажатии на кнопку
     if (check[articul] != undefined) {
         check[articul]++;
@@ -51,23 +54,46 @@ function addToCheck() {
     }
     // сохранение в localStorage
     localStorage.setItem('check', JSON.stringify(check) );
+
     console.log(check);
-    showCheck();
+    showCheck()
 }
+
+// Проверка нашего чека в let check на наличие уже лежащих позиций
 function testCheck() {
     // Проверка на существующие позиции счета в localStorage
     if (localStorage.getItem('check') != null) {
+    // Достаем из локалстор значения в переменную let check 
         check = JSON.parse (localStorage.getItem('check'));
     }
 }
+// Визуализация работы локалстор ПРОМЕЖУТОЧНАЯ НЕ ЗАПУСКАТЬ
+// function showCheck() {
+//     let out = '';
+//     for (let i in check){
+//         out +='<p>' +i+ '</br> кол-во ' +check[i]+ '</br>';
+//     }
+//     $('.calc-check___list-add-drinks').html(out);
+// }
+
+$.getJSON('classicCoffee.json', function(data) {
+    let clasCoff = data;
+    // console.log(data);
+    // console.log(check);
+    showCheck();
+
+})
 
 function showCheck() {
     let out = '';
-    for (let key in check){
-        out+='<img src="'+classicCoffee[key].image+'">';
+    for (let i in check){
+        out += i +check[i]+ '</br>';
     }
-    $('.calc-check___list-add-drinks').html(out);
+    $('.card').html(out);
 }
+
+
+
 
 
 
